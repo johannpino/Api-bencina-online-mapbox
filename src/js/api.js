bbe19666831509303ad9d81ecb12dba3c560b924
johannpino <johannpino@gmail.com>
2018-10-06 01:58:24 -0300

    Modificamos url ssl

--- src/js/api.js
+++ src/js/api.js
@@ -1,7 +1,7 @@
 export class API{
     async obtenerDatos(){
         const key = '16d2c2a6b857d242df70128cb49a9b775ee1e04c';
-        const url = `http://cne.cloudapi.junar.com/api/v2/datastreams/BENCI-EN-LINEA-V2-80280/data.ajson/?auth_key=${key}`;
+        const url = `//cne.cloudapi.junar.com/api/v2/datastreams/BENCI-EN-LINEA-V2-80280/data.ajson/?auth_key=${key}`;
 
         const datos = await fetch(url);
         
